f7edceb03d29460f2b9d207dad6f7ad942e53393
NaufalAD13 <naufaladlidhiaurrahman@gmail.com>
2023-12-03 22:37:17 +0700

    Tugas String and Date

--- Member.java
+++ Member.java
@@ -3,10 +3,14 @@ public abstract class Member {
     String namaPelanggan;
     String nomorFaktur;
     String namaBarang;
+    String kodeBarang;
     Long hargaBarang;
     Long jumlahBarang;
     Long totalBayar;
-    
+    Long jumlahBeli;
+    String noHp;
+    String alamat;
+    String namaKasir="Naufal Adli Dhiaurrahman";    
 
 
 
